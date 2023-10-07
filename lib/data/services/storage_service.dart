68057af692468a5bb680c40266ac7b8516a33d02
B1loliddin <bilolbotirov000@gmail.com>
2023-10-07 12:42:32 +0500

    Added uploadFile to store service. Bilol

--- lib/data/services/storage_service.dart
+++ lib/data/services/storage_service.dart
@@ -1,5 +1,17 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 
 sealed class StoreService {
   static final storage = FirebaseStorage.instance;
+
+  static Future<String> uploadFile(File file, String folder) async {
+    final image = storage.ref(folder).child(
+        "image_${DateTime.now().toIso8601String()}${file.path.substring(file.path.lastIndexOf("."))}");
+    final task = image.putFile(file);
+
+    await task.whenComplete(() {});
+
+    return image.getDownloadURL();
+  }
 }
